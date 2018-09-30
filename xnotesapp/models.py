c844973485a62d89dd589f767061284c6319e75c
Adi_Pat <adipat247360@gmail.com>
2018-09-30 20:58:14 +0530

    models.py: Add ArrayField for labels

--- xnotesapp/models.py
+++ xnotesapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.postgres.fields import ArrayField
 import uuid
 
 # Create your models here.
@@ -9,5 +10,6 @@ class Note(models.Model):
     title = models.CharField(max_length=80, null=False) # Blank title is allowed
     content = models.TextField(blank=False) 
     color = models.CharField(max_length=8, blank=False) # Note color RRGGBB
+    labels = ArrayField(models.CharField(max_length=20), default=list(['all']))
     created = models.DateTimeField(null=False)
     last_edited = models.DateTimeField(null=False)
